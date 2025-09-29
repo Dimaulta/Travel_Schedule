e63e9ddf44dfcddacfc93b1e3a2f09bb3517ba86
Ульта <lightpaintru@gmail.com>
2025-09-29 11:41:41 +0300

    Initial Commit

---
+++ Travel Schedule/Travel_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_ScheduleApp.swift
+//  Travel Schedule
+//
+//  Created by Ульта on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Travel_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
